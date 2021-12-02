4af354adc650a8939f30780a9a6661d0dcc5b996
Connor Robertson <cnr.robertson@gmail.com>
2021-12-01 21:30:04 -0500

    Add ifft

--- src/fft.jl
+++ src/fft.jl
@@ -16,3 +16,18 @@ function fft(x)
         return X
     end
 end
+
+function ifft(x)
+    N = length(x)
+    if N == 1
+        return x
+    else
+        X_even = ifft(x[1:2:N])
+        X_odd = ifft(x[2:2:N])
+        factor = exp.((2im*pi) .* ((0:(N-1)) ./ N))
+
+        N2 = N ÷ 2
+        X = [X_even .+ (factor[1:N2] .* X_odd); X_even .+ (factor[N2+1:N] .* X_odd)]
+        return X ./ 2
+    end
+end
--- test/test_fft.jl
+++ test/test_fft.jl
@@ -1,8 +1,3 @@
-# cd(@__DIR__)
-# using Pkg
-# Pkg.activate("../")
-# include("../src/nufft.jl")
-
 sr = 256
 ts = 1.0/(sr-1)
 t = 0:ts:1
@@ -16,21 +11,14 @@ x .+= sin.((2pi*freq) .* t)
 freq = 7.0
 x .+= 0.5 .* sin.((2pi*freq) .* t)
 
+# Test fft
 X = nufft.fft(x)
 sizes = reverse(sortperm(abs.(X)))
 @test sizes[1] == 2 || sizes[2] == 2
 @test sizes[3] == 5 || sizes[4] == 5
 @test sizes[5] == 8 || sizes[6] == 8
 
-# # For plotting
-# using CairoMakie
-
-# # Plot data
-# lines(t, x)
-
-# # Plot result
-# N = length(X)
-# n = 0:(N-1)
-# T = N/(sr-1)
-# freq = n ./ T
-# stem(freq, abs.(X))
+# Test ifft
+X = nufft.fft(x)
+new_x = real.(nufft.ifft(X))
+@test maximum(abs.(x .- new_x)) < 1e-12
