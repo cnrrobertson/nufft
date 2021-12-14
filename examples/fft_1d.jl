4acdb6bf05c87a6a53d244b7dba4393fe89f6f9b
Connor Robertson <cnr.robertson@gmail.com>
2021-12-13 22:20:13 -0500

    FFT examples

--- examples/fft_1d.jl
+++ examples/fft_1d.jl
@@ -3,6 +3,9 @@ using Pkg
 Pkg.activate("../")
 include("../src/nufft.jl")
 
+using Plots
+
+# %%
 sr = 256
 ts = 1.0/(sr-1)
 t = 0:ts:1
@@ -19,25 +22,24 @@ x .+= 0.5 .* sin.((2pi*freq) .* t)
 X = nufft.fft(x)
 sizes = reverse(sortperm(abs.(X)))
 
-# For plotting
-using CairoMakie
-
+# %%
 # Plot result
 N = length(X)
 n = 0:(N-1)
 T = N/(sr-1)
 freq = n ./ T
-stem(freq, abs.(X))
+P = plot(freq, abs.(X), line=:stem)
+display(P)
 
+# %%
 # Plot data
-lines(t, x, label="True")
+P = plot(t, x, label="True")
 approx = real.(nufft.ifft(X))
-lines!(t, approx, label="ifft(fft(true))")
-axislegend()
-current_figure()
+plot!(t, approx, label="ifft(fft(true))")
+display(P)
 
+# %%
 # Plot difference
 approx = real.(nufft.ifft(X))
-lines(t, abs.(x .- approx), label="abs difference")
-axislegend()
-current_figure()
+P = plot(t, abs.(x .- approx), label="abs difference")
+display(P)
